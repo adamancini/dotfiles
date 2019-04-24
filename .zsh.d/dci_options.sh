c1c2eb95cb88b3fe54ff56741cf56ec8f7ea7e70
ada <adab3ta@gmail.com>
2019-04-24 17:53:52 -0400

    disable development dci settings

--- .zsh.d/dci_options.sh
+++ .zsh.d/dci_options.sh
@@ -1,6 +1,6 @@
-export DCI_CLOUD=aws
+# export DCI_CLOUD=aws
 
 ## for local development
-export DCI_REPOSITORY=dockereng
+# export DCI_REPOSITORY=dockereng
 # export DCI_TAG=${DCI_CLOUD}-local
 # export DCI_TAG=aws
