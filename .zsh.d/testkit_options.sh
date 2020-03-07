7b5667f04e5aea2295e9bb1b646d480f34d1f67e
ada <adab3ta@gmail.com>
2020-03-07 00:21:10 -0500

    .zsh.d stuff

--- .zsh.d/testkit_options.sh
+++ .zsh.d/testkit_options.sh
@@ -1,18 +1,18 @@
-export TESTKIT_AWS_DEFAULT_REGION=us-east-1
-export TESTKIT_AWS_KEYNAME=amancini
+export TESTKIT_AWS_DEFAULT_REGION=us-east-2
+export TESTKIT_AWS_KEYNAME=amancini-jane
 export TESTKIT_AWS_PURCHASE_TYPE=spot
-export TESTKIT_AWS_REGION=us-east-1
-#export TESTKIT_AWS_SECURITY_GROUP=sg-0e07f43528941f940
-#export TESTKIT_AWS_SUBNET=subnet-0ea04ef42fb7b7304
-#export TESTKIT_AWS_VPC_ID=vpc-04897724c62ebed5d
+export TESTKIT_AWS_REGION=us-east-2
+export TESTKIT_AWS_SECURITY_GROUP=sg-0c7c91058c0f77bdb
+export TESTKIT_AWS_SUBNET=subnet-0021c861e0c8b3b9b
+export TESTKIT_AWS_VPC_ID=vpc-0b41997b6efbc63fc
 export TESTKIT_PROJECT_TAG=SUPPORT
 export TESTKIT_ENGINE=master
 export TESTKIT_DRIVER=aws
-export TESTKIT_SSH_KEYPATH=/home/$USER/.ssh/id_rsa
+export TESTKIT_SSH_KEYPATH=/home/$USER/.ssh/amancini-jane.pem
 
 function update_testkit {
-docker pull dockereng/testkit:latest \
- && id=$(docker create dockereng/testkit:latest) \
+docker pull mirantiseng/testkit:latest \
+ && id=$(docker create mirantiseng/testkit:latest) \
  && sudo docker cp $id:/testkit-$(uname -s)-$(uname -m) /usr/local/bin/testkit \
  && (docker rm $id >/dev/null)
 }
