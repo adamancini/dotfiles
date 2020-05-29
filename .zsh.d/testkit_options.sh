7159aecb79d4eaded1acb4623bb1ad4a3a7adffc
ada mancini <adab3ta@gmail.com>
2020-05-28 20:47:36 -0400

    move everything

--- .zsh.d/testkit_options.sh
+++ .zsh.d/testkit_options.sh
@@ -1,15 +1,9 @@
 export TESTKIT_AWS_REGION=us-east-2
 export TESTKIT_AWS_KEYNAME=amancini
-export TESTKIT_AWS_PURCHASE_TYPE=spot
-export TESTKIT_AWS_REGION=us-east-2
-export TESTKIT_AWS_SECURITY_GROUP=sg-0f7804ddec61629c0
-export TESTKIT_AWS_SUBNET=subnet-0e0b4d63243a17f0c
-# export TESTKIT_AWS_VPC_ID=vpc-0b41997b6efbc63fc
+export TESTKIT_AWS_PURCHASE_TYPE=ondemand
 export TESTKIT_PROJECT_TAG=SUPPORT
-# export TESTKIT_ENGINE=master
 export TESTKIT_DRIVER=aws
 export TESTKIT_SSH_KEYPATH=/home/$USER/.ssh/amancini.pem
-export TESTKIT_AWS_PURCHASE_TYPE=spot
 
 function update_testkit {
 docker pull mirantiseng/testkit:latest \
