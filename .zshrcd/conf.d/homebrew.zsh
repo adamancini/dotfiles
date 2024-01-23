18e41a4b8536afc46abbd0be731c5f8a1f1caad8
ada mancini <adab3ta@gmail.com>
2024-01-23 13:40:00 -0500

    homebrew bootstrap

---
+++ .zshrcd/conf.d/homebrew.zsh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if the operating system is macOS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # Check if Homebrew is installed
+    if ! command -v brew >/dev/null 2>&1; then
+        echo "Homebrew not found. Installing Homebrew."
+
+        # Install Homebrew
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+        echo "Homebrew installation complete."
+    fi
+fi
+
