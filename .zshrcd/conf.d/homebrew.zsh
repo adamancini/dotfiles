1e20867f86802ba095aaf3ff4a09ff3179a12adc
ada mancini <adab3ta@gmail.com>
2024-03-08 12:43:53 -0500

    homebrew stuff

--- .zshrcd/conf.d/homebrew.zsh
+++ .zshrcd/conf.d/homebrew.zsh
@@ -1,15 +1,14 @@
-#!/bin/bash
-
 # Check if the operating system is macOS
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    # Check if Homebrew is installed
-    if ! command -v brew >/dev/null 2>&1; then
-        echo "Homebrew not found. Installing Homebrew."
+  # Check if Homebrew is installed
+  if ! command -v brew >/dev/null 2>&1; then
+    echo "Homebrew not found. Installing Homebrew."
 
-        # Install Homebrew
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-        echo "Homebrew installation complete."
-    fi
+    echo "Homebrew installation complete."
+  fi
+  export  HOMEBREW_BUNDLE_FILE_GLOBAL="${ZCONFDIR}/Brewfile"
 fi
 
