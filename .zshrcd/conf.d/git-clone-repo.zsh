c67787a7a2c8f74d8a7bcceaf5ef8997ddfc82a4
ada mancini <ada@replicated.com>
2026-02-06 17:38:45 -0500

    Sync settings and zsh config changes

--- .zshrcd/conf.d/git-clone-repo.zsh
+++ .zshrcd/conf.d/git-clone-repo.zsh
@@ -1,45 +1,62 @@
 #!/usr/bin/env zsh
 
 # shell function that will git clone a repo url and add it to $SRC_HOME
-# based on the repo username and repo name, e.g. ~/src/github.com/username/repo
+# based on the domain, username, and repo name, e.g. ~/src/domain.com/username/repo
 # usage: git-clone-repo <repo_url>
-# example: git-clone-repo git@github.com:adamancini/dotfiles
+# examples:
+#   git-clone-repo git@github.com:adamancini/dotfiles
+#   git-clone-repo https://github.com/adamancini/dotfiles.git
+#   git-clone-repo https://akkoma.dev/AkkomaGang/akkoma.git
+#   git-clone-repo git@gitlab.com:username/project.git
 
-# this function is meant to be sourced from your .bashrc or .bash_profile
+# this function is meant to be sourced from your .zshrc
 # and aliased to a shorter command, e.g. alias gcr=git-clone-repo
 
-# if $SRC_HOME is not set, it will default to ~/src/github.com
-# if SRC_HOME does not exist, it will be created
+# if $SRC_HOME is not set, it will default to ~/src
+# the directory structure will be $SRC_HOME/domain/username/repo
 
 git-clone-repo() {
   setopt local_options BASH_REMATCH
   setopt local_options KSH_ARRAYS
   local repo_url=$1
+  local repo_domain
   local repo_username
   local repo_name
-  local src_home=${SRC_HOME:-$HOME/src/github.com}
+  local src_home=${SRC_HOME:-$HOME/src}
 
-  # Check if SRC_HOME exists, create it if not
-  if [[ ! -d $src_home ]]; then
-    mkdir -p "$src_home"
-  fi
-
-  # Extract repo username and repo name from repo url
+  # Extract domain, username, and repo name from repo url
   # Support both SSH and HTTPS formats, with or without .git extension
-  if [[ $repo_url =~ ^git@github.com:([^/]+)/(.+)(\.git)?$ ]]; then
-    repo_username=${BASH_REMATCH[1]}
-    repo_name=${BASH_REMATCH[2]%.git}  # Remove .git if present
-    # echo "$repo_username/$repo_name"
-  elif [[ $repo_url =~ ^https://github.com/([^/]+)/(.+)(\.git)?/?$ ]]; then
-    repo_username=${BASH_REMATCH[1]}
-    repo_name=${BASH_REMATCH[2]%.git}  # Remove .git if present
-    # echo "$repo_username/$repo_name"
+  if [[ $repo_url =~ ^git@([^:]+):([^/]+)/(.+)(\.git)?$ ]]; then
+    # SSH format: git@domain:username/repo.git
+    repo_domain=${BASH_REMATCH[1]}
+    repo_username=${BASH_REMATCH[2]}
+    repo_name=${BASH_REMATCH[3]%.git}  # Remove .git if present
+  elif [[ $repo_url =~ ^https://([^/]+)/([^/]+)/(.+)(\.git)?/?$ ]]; then
+    # HTTPS format: https://domain/username/repo.git
+    repo_domain=${BASH_REMATCH[1]}
+    repo_username=${BASH_REMATCH[2]}
+    repo_name=${BASH_REMATCH[3]%.git}  # Remove .git if present
   else
     echo "Invalid git url: $repo_url"
+    echo "Supported formats:"
+    echo "  SSH:   git@domain:username/repo.git"
+    echo "  HTTPS: https://domain/username/repo.git"
     return 1
   fi
 
+  # Construct the destination path
+  local dest_path="$src_home/$repo_domain/$repo_username/$repo_name"
+
+  # Check if destination directory exists
+  if [[ -d $dest_path ]]; then
+    echo "Directory already exists: $dest_path"
+    return 1
+  fi
+
+  # Create parent directory if it doesn't exist
+  mkdir -p "$(dirname "$dest_path")"
+
   # Clone the repo and add it to SRC_HOME
-  echo "git clone $repo_url $src_home/$repo_username/$repo_name"
-  git clone $repo_url $src_home/$repo_username/$repo_name
+  echo "git clone $repo_url $dest_path"
+  git clone $repo_url $dest_path
 }
