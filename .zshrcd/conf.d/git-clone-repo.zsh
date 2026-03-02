c370ecdefea2243c685d61dfe9bd37e7fe675444
ada mancini <ada@replicated.com>
2026-03-02 09:28:32 -0500

    Resolve merge conflict in settings.json, sync local changes
    Merged remote trimmed plugin list with local additions
    (ys-skill, replicated-review, double-shot-latte).
    Updated known_marketplaces.json timestamps.

--- .zshrcd/conf.d/git-clone-repo.zsh
+++ .zshrcd/conf.d/git-clone-repo.zsh
@@ -2,12 +2,11 @@
 
 # shell function that will git clone a repo url and add it to $SRC_HOME
 # based on the domain, username, and repo name, e.g. ~/src/domain.com/username/repo
-# usage: git-clone-repo <repo_url>
+# usage: git-clone-repo [--cd] <repo_url>
 # examples:
 #   git-clone-repo git@github.com:adamancini/dotfiles
 #   git-clone-repo https://github.com/adamancini/dotfiles.git
-#   git-clone-repo https://akkoma.dev/AkkomaGang/akkoma.git
-#   git-clone-repo git@gitlab.com:username/project.git
+#   git-clone-repo --cd git@gitlab.com:username/project.git
 
 # this function is meant to be sourced from your .zshrc
 # and aliased to a shorter command, e.g. alias gcr=git-clone-repo
@@ -18,11 +17,26 @@
 git-clone-repo() {
   setopt local_options BASH_REMATCH
   setopt local_options KSH_ARRAYS
-  local repo_url=$1
+  local change_dir=false
+  local repo_url
   local repo_domain
   local repo_username
   local repo_name
   local src_home=${SRC_HOME:-$HOME/src}
+
+  # Parse options
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+      --cd) change_dir=true; shift ;;
+      -*) echo "Unknown option: $1"; return 1 ;;
+      *) repo_url=$1; shift ;;
+    esac
+  done
+
+  if [[ -z $repo_url ]]; then
+    echo "Usage: git-clone-repo [--cd] <repo_url>"
+    return 1
+  fi
 
   # Extract domain, username, and repo name from repo url
   # Support both SSH and HTTPS formats, with or without .git extension
@@ -58,5 +72,9 @@ git-clone-repo() {
 
   # Clone the repo and add it to SRC_HOME
   echo "git clone $repo_url $dest_path"
-  git clone $repo_url $dest_path
+  git clone $repo_url $dest_path || return 1
+
+  if $change_dir; then
+    cd "$dest_path"
+  fi
 }
