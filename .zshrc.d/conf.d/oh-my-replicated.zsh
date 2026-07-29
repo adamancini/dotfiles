76a47c68455fd86e010cc6d870af83939ce4fdb3
Ada Mancini <ada.mancini@akuity.io>
2026-07-29 11:29:28 -0400

    Remove Replicated-specific tooling from shell config
    No longer needed now that I'm not at Replicated: drop the
    replicatedhq/replicated Homebrew tap and CLI formula, and the
    oh-my-replicated.zsh env-var loader.

--- .zshrc.d/conf.d/oh-my-replicated.zsh
+++
@@ -1,6 +0,0 @@
-# User-specific credentials loaded from ~/.zshenv.local (not tracked in yadm)
-# Add the following there:
-#   export GUSER='ada'
-#   export GPREFIX='ada'
-#   export AWSUSER='ada'
-#   export AWSPREFIX='ada'
